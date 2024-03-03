6850c5e3525402683cc5846165c8eb1ebe6cb95c
iskandarprogrammer2007 <abdumalikoviskandar600@gmail.com>
2024-03-03 11:06:24 +0500

    all pages

---
+++ js/main.js
@@ -0,0 +1,43 @@
+let backTop = document.querySelector(".back-top");
+let header = document.querySelector(".header");
+let body = document.querySelector("body");
+let headerList = document.querySelector(".header__nav__right__list");
+let hamburger = document.querySelector(".header__nav__right__btns__hamburger");
+let closeHeaderList = document.querySelector(".header__nav__right__list__close");
+let dropdown = document.querySelector(".dropdown")
+let dropdownList = document.querySelector(".dropdown__list")
+let dropdownIcon = document.querySelector(".dropdown__icon")
+
+
+dropdown.addEventListener("click",function(){
+  dropdownList.classList.toggle("show__dropdown__list");
+  dropdownIcon.classList.toggle("show__dropdown-icon");
+})
+
+window.addEventListener("scroll", function () {
+  showBackTop();
+  showHeaderShrink();
+});
+
+hamburger.addEventListener("click", function () {
+  headerList.classList.toggle("show__header__nav__right__list");
+});
+
+closeHeaderList.addEventListener("click", function () {
+  headerList.classList.remove("show__header__nav__right__list");
+});
+
+function showBackTop() {
+  if (scrollY > 100) {
+    backTop.classList.add("show__back-top");
+  } else {
+    backTop.classList.remove("show__back-top");
+  }
+}
+function showHeaderShrink() {
+  if (scrollY > 0) {
+    header.classList.add("show__header__shrink");
+  } else {
+    header.classList.remove("show__header__shrink");
+  }
+}
